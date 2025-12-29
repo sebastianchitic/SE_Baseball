7ba764c73b43351a9ce68f10b84b03dbf3b4c0e9
sebastianchitic <sebastianccdd@gmail.com>
2025-12-29 15:30:53 +0100

    modifica

--- UI/controller.py
+++ UI/controller.py
@@ -9,7 +9,18 @@ class Controller:
         self._model = model
         self._DAO = DAO
 
-    """ Altri possibili metodi per gestire di dd_anno """""
+
+    def handle_crea_grafo(self, e):
+        """ Handler per gestire creazione del grafo """""
+
+
+    def handle_dettagli(self, e):
+        """ Handler per gestire i dettagli """""
+
+
+    def handle_percorso(self, e):
+        """ Handler per gestire il problema ricorsivo di ricerca del percorso """""
+
 
     def popolaDropdown(self):
         years = self._model.get_all_years()
@@ -27,18 +38,6 @@ class Controller:
         for s in squadre:
             self._view.dd_squadra.controls.append(ft.dropdown.Option(key=s.teamCode, text=s.name))
         self._view.page.update()
-
-    def handle_crea_grafo(self, e):
-        """ Handler per gestire creazione del grafo """""
-        # TODO
-
-    def handle_dettagli(self, e):
-        """ Handler per gestire i dettagli """""
-        # TODO
-
-    def handle_percorso(self, e):
-        """ Handler per gestire il problema ricorsivo di ricerca del percorso """""
-        # TODO
 
 
 
